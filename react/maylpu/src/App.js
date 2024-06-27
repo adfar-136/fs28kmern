e6cc7a6c6993e2a0711eaefd497315158009d051
adfar-136 <adfarrasheed136@gmail.com>
2024-06-27 17:57:17 +0530

    useMemo

--- react/maylpu/src/App.js
+++ react/maylpu/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import ParentCompoent from './components/Refs/ParentCompoent'
-
+import React, { useState } from 'react'
+import TodoList from './components/memo/TodoList'
+import { createTodos } from './components/memo/utils'
+import "./App.css"
+let todos = createTodos()
 export default function App() {
+const [tab,setTab] = useState("all")
+const [isDark,setIsDark] = useState(false)
   return (
     <div>
-      <ParentCompoent/>
+      <button onClick={()=>setTab("all")}>All</button>
+      <button onClick={()=>setTab("active")}>Active</button>
+      <button onClick={()=>setTab("completed")}>Completed</button>
+      <br />
+      <label>
+        <input type="checkbox" value="" 
+        onChange={(e)=>setIsDark(e.target.checked)}/>
+        Dark Mode
+      </label>
+      <TodoList
+       todos = {todos}
+       tab = {tab}
+       theme = {isDark ?  "dark" : "light"}
+      />
     </div>
   )
 }
